51b5541d1063b195662224d314ea339b4a908dcb
Patrick N <pdn4kd@noreply.github.com>
2015-04-10 21:24:44 -0500

    messing w/ classes

--- ascent.py
+++ ascent.py
@@ -1,24 +1,33 @@
-# Ascent Simulation 0.3 -- Feb 10, 2015
+# Ascent Simulation 0.3 -- Apr 10, 2015
 
-from math import pi, exp, sin, cos, sqrt
+from math import pi, exp, sin, cos, sqrt, log
 
 class Body:
 	#Kerbin by default
 	radius = 600000.0
 	mu = 3531600000000.0 #gravitational parameter
 	scale = 5000.0 #atmospheric scale height
+	cutoff = 5000.0*log(1e6) #atmospheric cutoff altitude
 	rot = 2*pi/21600 #rotation rate in rad/s. Because the 6 h sidereal day is still real.
 	k = 0.004 * 1.2230948554874 #magic number to make drag work based on local pressure. Includes the 0.5 factor in the drag equation. (uses data from the actual API)
+	def pressure(alt):
+		if (alt < cutoff):
+			return exp(-alt/scale)
+		else:
+			return 0.0
 
 class Craft:
+	#single stage only, example craft is mk1 pod + mk16 chute + RT-10 SRB
 	Thrust = 250
-	MassWet = 0.1+0.8+0.5+0.05+1.125
-	MassDry = MassEmpty + fuel*0.0075
+	MassDry = 0.1+0.8+0.5
+	MassWet = 1.4 + 433*0.0075
+	CdDry = (0.1*0.22+0.8*0.2+0.5*0.3)/1.4
+	CdWet = (0.1*0.22+0.8*0.2+3.7475*0.3)/4.6475
 	Isp1 = 225
 	Isp0 = 240
-	def Isp(Body.pressure):
-		If Body.Pressure(alt) > 0:
-			return Isp0 - (Isp0 - Isp1)*Body.Pressure(alt)
+	def Isp(pressure):
+		if pressure > 0:
+			return Isp0 - (Isp0 - Isp1)*pressure
 		else:
 			return Isp0
 #class ascentProfile:
@@ -33,16 +42,15 @@ class Craft:
 	def stop():
 		csv.close()
 
-class OrbitalParameters:
-	semiMajorAxis
-	eccentricity
-	apoapsis
-	periapsis
-	update(R, Spd):
-		semiMajorAxis = 1/(2/R - Spd*Spd/Body.GM)
-		eccentricity = Math.sqrt(1-R*R*Spd*Spd/(semiMajorAxis*Body.GM))
-		apoapsis = semiMajorAxis*(1+eccentricity)
-		periapsis = semiMajorAxis*(1-eccentricity) 
+class Orbit:
+	def semiMajorAxis(R, Spd, GM):
+		return 1/(2/R - Spd*Spd/GM)
+	def eccentricity(R, Spd, GM, SMA):
+		return sqrt(1-R*R*Spd*Spd/(SMA*GM))
+	def apoapsis(semiMajorAxis, eccentricity):
+		return semiMajorAxis*(1+eccentricity)
+	def periapsis(semiMajorAxis, eccentricity):
+		return semiMajorAxis*(1-eccentricity) 
 
 fuel = 433.0
 Throttle = 1.0
@@ -58,8 +66,9 @@ while (Throttle > 0.0):
 	m = MassFull
 
 	#starting situation
-	r = radius + 5000.0
-	altitude = 0.0
+	Kerbin = Body
+	altitude = 70.0
+	r = Kerbin.radius + altitude
 	theta = 0.0
 	dr = 0.0
 	dtheta = 0.0
@@ -70,31 +79,33 @@ while (Throttle > 0.0):
 	tmax = 100.0
 	altmax = 0.0
 	dv = 0.0
+	Kerbin.pressure()
+
 	#~ csv.write(str(t) + "," + str(m) + "," + str(r) + "," + str(dr) + "," + str(ddr) + "," + str(theta) + "," + str(dtheta) + "," + str(ddtheta) + "\r\n")
 
 	#need to account for planet rotation with theta & rot
-	while ((t < tmax) and (r > radius)):
-		altitude = r - radius
-		pressure = exp(-altitude/scale)
+	while ((t < tmax) and (r >= Kerbin.radius)):
+		altitude = r - Kerbin.radius
+		Kerbin.pressure(altitude)
 		Cd = ((0.182) + 0.3*(m-0.9))/m
 		r += dr*dt+0.5*ddr*dt*dt
 		theta += dtheta*dt+0.5*ddtheta*dt*dt
 		dr += ddr*dt
 		dtheta += ddtheta*dt
 		# v = sqrt(dr*dr+r*r*dtheta*dtheta) #going to confine drag to seperate r and theta parts.
-		ddr = (r*dtheta*dtheta) - (k*Cd*pressure*dr*abs(dr)) - (mu/r/r) + (Throttle*Thrust*sin(gamma)/m)
-		ddtheta = (-2*dr*dtheta/r) - (k*Cd*pressure*r*dtheta*abs(r*dtheta)) + (Throttle*Thrust*cos(gamma)/m)
+		ddr = (r*dtheta*dtheta) - (Kerbin.k*Cd*Kerbin.pressure(altitude)*dr*abs(dr)) - (mu/r/r) + (Throttle*Thrust*sin(gamma)/m)
+		ddtheta = (-2*dr*dtheta/r) - (Kerbin.k*Cd*Kerbin.pressure(altitude)*r*dtheta*abs(r*dtheta)) + (Throttle*Thrust*cos(gamma)/m)
 		if ((Thrust > 0) and (Throttle > 0)):
 			dv += dt*Thrust*Throttle/m
 		if (m == MassEmpty):
 			dm = 0
 			Thrust = 0
 		if (m > MassEmpty):
-			dm = Throttle*Thrust/((Isp0 - (Isp0 - Isp1)*pressure)*9.82)
+			dm = Throttle*Thrust/((Isp0 - (Isp0 - Isp1)*Kerbin.pressure(altitude))*9.82)
 		m -= dm*dt
 		if (m < MassEmpty): #out of propellant
 			m = MassEmpty
-			Thrust = dm*(Isp0 - (Isp0 - Isp1)*pressure)*9.82 #I'm pretty sure KSP doesn't do this.
+			Thrust = dm*(Isp0 - (Isp0 - Isp1)*Kerbin.pressure(altitude))*9.82 #I'm pretty sure KSP doesn't do this.
 		t += dt
 		if (altmax < altitude):
 			altmax = altitude
